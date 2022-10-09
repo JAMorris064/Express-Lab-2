4fad2988abfadf0584d9c43da9499c8f5e2c4533
JAMorris064 <112364946+JAMorris064@users.noreply.github.com>
2022-10-08 20:12:37 -0500

    Tip Calculator express application created.

--- express2.js
+++ express2.js
@@ -13,6 +13,13 @@ app.get('/greeting/:name', (req, res) => {
     res.send(`Nice to meet you, ${req.params.name}!`)
 })
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    let total = parseInt(req.params.total)
+    let tipPercentage = parseInt(req.params.tipPercentage)
+    console.log(tipPercentage * total / 100)
+    res.send(`${tipPercentage * total / 100}`)
+})
+
 app.listen(3000, () => {
     console.log(`You are listening to Port 3000...`)
 })
