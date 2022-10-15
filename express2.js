39c1e09e37072f4f999fdfeb0ec1965f2f15adf4
JAMorris064 <112364946+JAMorris064@users.noreply.github.com>
2022-10-15 12:20:34 -0500

    Magic Eight Ball

--- express2.js
+++ express2.js
@@ -20,6 +20,37 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send(`${tipPercentage * total / 100}`)
 })
 
+const eightBall = [
+ "It is certain",
+ "It is decidedly so", 
+ "Without a doubt", 
+ "Yes definitely",
+ "You may rely on it", 
+ "As I see it yes", 
+ "Most likely", 
+ "Outlook good",
+ "Yes", 
+ "Signs point to yes", 
+ "Reply hazy try again", 
+ "Ask again later",
+ "Better not tell you now", 
+ "Cannot predict now", 
+ "Concentrate and ask again",
+ "Don't count on it",
+ "My reply is no",
+ "My sources say no",
+ "Outlook not so good", 
+ "Very doubtful"
+]
+
+
+// app.get(/)
+// var magic = 
+ app.get('/magic', (req, res) =>{
+ res.send(`${req}`)
+ res.send(eightBall[req.params.magic]   
+
+          
 app.listen(3000, () => {
     console.log(`You are listening to Port 3000...`)
 })
